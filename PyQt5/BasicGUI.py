6a41a0b52fbfb19bf8ab55274d6766c796525492
EddieNine <believefixed@hotmail.com>
2024-08-01 12:34:53 -0300

    Learning PyQt5

--- PyQt5/BasicGUI.py
+++ PyQt5/BasicGUI.py
@@ -1,6 +1,7 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt5.QtGui import QIcon, QFont
+from PyQt5.QtCore import Qt
 
 
 class MainWindow(QMainWindow):
@@ -9,6 +10,19 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("My cool first GUI")
         self.setGeometry(700, 300, 500, 500)
         self.setWindowIcon(QIcon("dog-house.png"))
+
+        label = QLabel("Hello", self)
+        label.setFont(QFont("Arial", 40))
+        label.setGeometry(0, 0, 500, 100)
+        label.setStyleSheet("color: blue;"
+                            "background-color: green;"
+                            "font-weight: bold;"
+                            "font-style: italic;"
+                            "text-decoration: underline;")
+
+        # label.setAlignment(Qt.AlignTop) # Vertically top
+        # label.setAlignment(Qt.AlignBottom) # Vertically bottom
+        label.setAlignment(Qt.AlignCenter)
 
 
 def main():
